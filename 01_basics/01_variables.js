12001b18b853468582d79f1f350f92f31f95e192
ITz-ABHISHEK <abhishek0211raj@gmail.com>
2025-07-16 05:18:24 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 7677368
+let accountEmail = "abhishek@gmail.com"
+var accountPassword = "767845"
+accountCity = "Rishikesh"
+
+// accountId = 2 // not allowed
+
+accountEmail= "raj7438@gmail.com"
+accountPassword= "98326"
+accountCity= "jaipur"
+let accountState ;
+
+/*dont use var bcoz of issue in block scope and functional scope*/
+console.table([accountId, accountEmail, accountPassword ,accountCity, accountState])
