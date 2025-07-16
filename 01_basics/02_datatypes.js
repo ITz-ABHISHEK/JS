35351c83646fd951e989e92c8b7118b01d790e70
ITz-ABHISHEK <abhishek0211raj@gmail.com>
2025-07-16 05:19:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js cose as newer version
+
+//alert (8+7) // we are using nodejs, not browser
+
+console.log(7
+    +9)//code readiability should be high
+console.log("ABHISHEK")
+
+let name = "abhishek"
+let age = 20
+let isloggedIn = false
+
+
+//nameber=> 2 to 53
+//bigint
+// string ==> ""
+//boolean => true /false
+//null=> standalone value
+//undefined=>
+// symbol => unique
+//object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
