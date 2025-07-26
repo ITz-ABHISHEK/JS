39094a01c850d6b49de334040ee1c848214c3e45
ITz-ABHISHEK <abhishek0211raj@gmail.com>
2025-07-26 08:34:22 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "ABHISHEK"
+const repoCount = 05
+
+//console.log(name + repoCount + "value");
+
+console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('abhishekraj')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
